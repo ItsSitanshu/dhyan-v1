a53c6c2eaf3457bc5501c2e09e191169e5b06880
Pranjal <devpranjalpanthi707@gmail.com>
2025-02-11 21:04:56 +0545

    aashugtoribhai

---
+++ app/simulations/ElectricCircuit.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import React, { useState } from "react";
+
+const ElectricCircuit = () => {};
+
+export default ElectricCircuit;
--- app/simulations/page.tsx
+++ app/simulations/page.tsx
@@ -1,11 +1,13 @@
 import CollisionFiltering from "./NewtonSecondLaw";
 import ProjectileMotion from "./ProjectileMotion";
 import FrictionSimulation from "./Friction";
+import ElectricCircuit from "./ElectricCircuit";
 import Orbitals from "./Orbitals";
 
 const Simulation = () => {
-  return(
-    <CollisionFiltering />
+  return (
+    <ElectricCircuit />
+    // <CollisionFiltering />
     // <ProjectileMotion />
     // <FrictionSimulation />
     // <Orbitals />
