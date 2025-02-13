97024a84186d9aa4e83eb12e18857b31eea7d0ff
baruwalaashug <spider16man10@gmail.com>
2025-02-13 19:48:24 +0545

    sab kam maile gare

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="w-full max-w-5xl flex justify-between items-center p-6">
+    <header className="w-full max-w-5xl flex justify-between items-center p-6 border-b-2">
       <div className="flex items-center">
         <img alt="logo" className="h-10" src="/logo.svg" />
         <span className="ml-2 mt-1 text-3xl font-semibold font-nue">Dhyan.AI</span>
@@ -12,7 +12,7 @@ const Header = () => {
           <a
             key={index}
             href="#"
-            className="relative text-foreground after:block after:content-[''] after:absolute after:h-[2px] after:bg-foreground after:w-0 after:left-0 after:bottom-[-2px] after:transition-all after:duration-300 hover:after:w-full"
+            className="relative text-foreground after:block after:content-[''] after:absolute after:h-[2px] after:bg-white after:w-0 after:left-0 after:bottom-[-2px] after:transition-all after:duration-300 hover:after:w-full text-white"
           >
             {item}
           </a>
